ed49792e0d47b6af2d0518a78926c8506534316e
Rishabh Khanna <rishabhrrk@gmail.com>
2019-11-23 16:05:05 -0500

    initial commit

---
+++ lib/twitter_engine.ex
@@ -0,0 +1,18 @@
+defmodule TwitterEngine do
+  @moduledoc """
+  Documentation for TwitterEngine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwitterEngine.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
